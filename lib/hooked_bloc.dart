20355869bb96551ef8b21bbbfa7d2bcbd5013694
Andrzej Nowak <andrzej.nowak@iteo.com>
2022-03-15 11:12:27 +0100

    export missing bloc_action_listener hook

--- lib/hooked_bloc.dart
+++ lib/hooked_bloc.dart
@@ -4,6 +4,7 @@ export 'src/bloc_hook.dart';
 export 'src/bloc_builder.dart';
 export 'src/bloc_listener.dart';
 export 'src/build_state.dart';
+export 'src/bloc_action_listener.dart';
 export 'src/hooked_bloc.dart';
 export 'src/mixins/bloc_action_mixin.dart';
 export 'src/bloc/action_bloc.dart';
