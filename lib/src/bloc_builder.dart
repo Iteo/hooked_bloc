7d695a535a36df8a58bc4de04ea26e2f1433a916
anowakiteo <67737679+anowakiteo@users.noreply.github.com>
2022-03-24 09:45:50 +0100

    add comment bloc for useCubitBuilder (#30)

--- lib/src/bloc_builder.dart
+++ lib/src/bloc_builder.dart
@@ -1,9 +1,14 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooked_bloc/hooked_bloc.dart';
 import 'package:hooked_bloc/src/injection/hook_injection_controller.dart';
 
 typedef BlocBuilderCondition<S> = bool Function(S current);
 
+/// Returns current state of BlocBase class
+///
+/// By default [useCubitBuilder] will not filter any states, unless specified [buildWhen] parameter or [builderCondition] in [HookedBloc.initialize]
+/// [buildWhen] local filter function, that will filter incoming states from [BlocBase.stream]
 S useCubitBuilder<C extends BlocBase, S>(
   BlocBase<S> cubit, {
   BlocBuilderCondition<S>? buildWhen,
