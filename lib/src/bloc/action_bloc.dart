0ad214f48580508b6ac8e34ce73af067b564607c
anowakiteo <67737679+anowakiteo@users.noreply.github.com>
2022-03-24 13:57:36 +0100

    add comment blocks for ActionCubit and ActionBloc (#35)

--- lib/src/bloc/action_bloc.dart
+++ lib/src/bloc/action_bloc.dart
@@ -1,6 +1,21 @@
 import 'package:hooked_bloc/src/mixins/bloc_action_mixin.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+/// Syntactic sugar for [Bloc] with [BlocActionMixin]
+///
+/// [BlocActionMixin] is useful while adding events to the existing Cubits
+/// Creating new Cubit with actions stream, can be done by extending [ActionCubit]:
+///
+///```dart
+/// class MessageActionBloc extends ActionBloc<BuildState, int, String>{
+///   MessageActionBloc(BuildState initialState) : super(initialState);
+///
+///   @override
+///   void dispatch(String action) {
+///     super.dispatch(action);
+///   }
+/// }
+///```
 abstract class ActionBloc<STATE extends Object, EVENT, ACTION>
     extends Bloc<EVENT, STATE> with BlocActionMixin<ACTION, STATE> {
   ActionBloc(STATE initialState) : super(initialState);
