3689723e779d5ddb92a79f3ce10d34503d934e22
anowakiteo <67737679+anowakiteo@users.noreply.github.com>
2022-03-24 06:18:31 +0100

    add useCubit code documentation (#29)

--- lib/src/bloc_hook.dart
+++ lib/src/bloc_hook.dart
@@ -3,6 +3,12 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooked_bloc/src/cubit_defaults.dart';
 import 'package:hooked_bloc/src/injection/hook_injection_controller.dart';
 
+/// Provides BlocBase<T> class.
+///
+/// [useCubit] will find and return [BlocBase] class using injector provided by [BlocHookInjectionController]
+/// If [BlocHookInjectionController] has no provided injectors, [useCubit] will look into widget tree and try find BlocBase using [BlocProvider]
+///
+/// [useCubit] will call automatically [BlocBase.close] method. If needed, this behaviour can be changed by setting [closeOnDispose] flag to false
 T useCubit<T extends BlocBase>({
   List<dynamic> keys = const <dynamic>[],
   bool closeOnDispose = true,
