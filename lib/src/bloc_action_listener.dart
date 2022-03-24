37dc27064b07e549a68bf18434683af8bc865a2e
Andrzej Nowak <andrzej.nowak@iteo.com>
2022-03-24 08:06:26 +0100

    add comment block for useActionListener

--- lib/src/bloc_action_listener.dart
+++ lib/src/bloc_action_listener.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooked_bloc/src/mixins/bloc_action_mixin.dart';
+import 'package:hooked_bloc/hooked_bloc.dart';
 
 typedef OnActionCallback<T> = Function(T action);
 
+/// Calls callback function [onAction] each time, when new action is dispatched from [BlocBase] with [BlocActionMixin], [ActionCubit] or [ActionBloc]
 void useActionListener<ACTION>(
   BlocActionMixin<ACTION, Object> actionMixin,
   OnActionCallback<ACTION> onAction,
