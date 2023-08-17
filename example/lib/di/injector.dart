daf53e3f35a779ebb6f4081ea15f9302ad48cd24
Marcin Starmach <marcin.starmach@iteo.com>
2023-08-17 07:22:09 +0200

    update flutter hooks

--- example/lib/di/injector.dart
+++ example/lib/di/injector.dart
@@ -1,8 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-import 'package:example/di//injector.config.dart';
+import 'package:example/di/injector.config.dart';
 
 final getIt = GetIt.instance;
 
 @InjectableInit(preferRelativeImports: false)
-Future<void> configureDependencies() async => $initGetIt(getIt);
+Future<void> configureDependencies() async => getIt.init();
